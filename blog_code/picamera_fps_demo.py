8a082d51ae6ae207ec4423468949a24efbc87dc6
theo@pi3 <theostangebye@gmail.com>
2019-02-08 16:55:14 -0500

    increase threaded resolution and decrease cam fps to 10

--- blog_code/picamera_fps_demo.py
+++ blog_code/picamera_fps_demo.py
@@ -21,50 +21,10 @@ ap.add_argument("-n", "--num-frames", type=int, default=100,
 	help="# of frames to loop over for FPS test")
 args = vars(ap.parse_args())
 
-# initialize the camera and stream
-camera = PiCamera()
-camera.resolution = (1920, 1080)
-camera.framerate = 10
-rawCapture = PiRGBArray(camera, size=(1920, 1080))
-stream = camera.capture_continuous(rawCapture, format="bgr",
-	use_video_port=True)
-
-# allow the camera to warmup and start the FPS counter
-print("[INFO] sampling frames from `picamera` module...")
-time.sleep(2.0)
-fps = FPS().start()
-
-# loop over some frames
-for (i, f) in enumerate(stream):
-	# grab the frame from the stream and resize it to have a maximum
-	# width of 2000 pixels
-	frame = f.array
-	frame = imutils.resize(frame, width=2000)
-
-	# clear the stream in preparation for the next frame and update
-	# the FPS counter
-	rawCapture.truncate(0)
-	fps.update()
-
-	# check to see if the desired number of frames have been reached
-	if i == args["num_frames"]:
-		break
-
-# stop the timer and display FPS information
-fps.stop()
-print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-
-# do a bit of cleanup
-cv2.destroyAllWindows()
-stream.close()
-rawCapture.close()
-camera.close()
-
 # created a *threaded *video stream, allow the camera sensor to warmup,
 # and start the FPS counter
 print("[INFO] sampling THREADED frames from `picamera` module...")
-vs = PiVideoStream().start()
+vs = PiVideoStream(resolution=(1920,1080), framerate=32).start()
 time.sleep(2.0)
 fps = FPS().start()
 
